caa326396a5970dd11d82db9ac5401c1bfac46af
Saifuddin <makers.52@gmail.com>
2016-09-23 21:18:07 +0500

    Login Form App Completely Compiled

---
+++ LoginFormApp/app/src/main/java/com/example/maker/loginformapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.maker.loginformapp;
+
+        import android.content.Context;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
+        import android.widget.EditText;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                final EditText text = (EditText)findViewById(R.id.edittext);
+                Toast.makeText(MainActivity.this, text.getText() , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
